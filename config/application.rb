e07be4db54b4e0b5fd9e28f9c30ddda8e9dd5f05
yuya <yuya.skd@gmail.com>
2016-10-16 13:05:16 +0000

    add initial setups

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,11 @@ module Microposts
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
